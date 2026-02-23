7f1807e8a3ad3332abbb49792d3c8f09544a7a12
dev-sumon <akhtaruzzamansumon7@gmail.com>
2026-02-23 15:38:01 +0600

    some update

---
+++ resources/js/components/section/home/top-related.tsx
@@ -0,0 +1,83 @@
+import ServiceCard from '@/components/ui/service-card';
+
+const service = [
+    {
+        id: 1,
+        image: '/assets/images/service/EliteAutoSpa.png',
+        name: 'Elite Auto Spa',
+        rating: 4.9,
+        location: 'DETAILING',
+        service: 'See Details',
+        price: 120,
+    },
+    {
+        id: 1,
+        image: '/assets/images/service/EliteAutoSpa.png',
+        name: 'Elite Auto Spa',
+        rating: 4.9,
+        location: 'DETAILING',
+        service: 'See Details',
+        price: 120,
+    },
+    {
+        id: 1,
+        image: '/assets/images/service/EliteAutoSpa.png',
+        name: 'Elite Auto Spa',
+        rating: 4.9,
+        location: 'DETAILING',
+        service: 'See Details',
+        price: 120,
+    },
+    {
+        id: 1,
+        image: '/assets/images/service/EliteAutoSpa.png',
+        name: 'Elite Auto Spa',
+        rating: 4.9,
+        location: 'DETAILING',
+        service: 'See Details',
+        price: 120,
+    },
+    {
+        id: 1,
+        image: '/assets/images/service/EliteAutoSpa.png',
+        name: 'Elite Auto Spa',
+        rating: 4.9,
+        location: 'DETAILING',
+        service: 'See Details',
+        price: 120,
+    },
+    {
+        id: 1,
+        image: '/assets/images/service/EliteAutoSpa.png',
+        name: 'Elite Auto Spa',
+        rating: 4.9,
+        location: 'DETAILING',
+        service: 'See Details',
+        price: 120,
+    },
+];
+
+export default function TopRelated() {
+    return (
+        <div
+            className="relative bg-cover bg-center bg-no-repeat py-10"
+            style={{
+                backgroundImage: "url('/assets/images/bg.png')",
+            }}
+        >
+            {/* Overlay */}
+            <div className="absolute inset-0 bg-black/70"></div>
+            <div className="relative z-10 container flex h-full flex-col justify-center px-4">
+                {/* Card 1 */}
+                <h2 className="mb-5 text-3xl font-medium text-white">
+                   Top Rated Near You
+                </h2>
+                <div className="grid grid-cols-4 gap-9">
+                    {service.map((item) => (
+                        <ServiceCard key={item.id} {...item} />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
